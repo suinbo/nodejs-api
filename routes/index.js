133a43c8174845ff7d7d314c02a2084f9e3b94bd
suinbo <suin9610@gmail.com>
2023-06-01 00:11:19 +0900

    [Fix] api 응답 포맷 변경

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,20 @@ router.post('/api/post/login', function(req, res) {
   const { accessId, secretPw } = req.body
   if (accessId == "admin" && secretPw == "1234") {
     res.status(200).json({
-      "message": "login success."
+      headers: {},
+      data: {
+          accessToken: "admin",
+          expireTime: 1688207263,
+          languageCode: "ko",
+          loginId: "admin",
+          loginIp:"",
+          loginStatus: "S1",
+          loginTime: 1688207263,
+          regionList: [{id: "KR"}, {id: "JP"}],
+          timeZoneData: 1688207263
+      },
+      code: "0000",
+      detailMessage: "login success.",
     })
   } else {
     res.status(200).json({
