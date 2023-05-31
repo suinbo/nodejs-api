329e02ca51290dc5b8459dd0da9bb98af6b7fd1b
suinbo <suin9610@gmail.com>
2023-05-31 19:14:31 +0900

    [Fix] post api 엔드포인트 변경

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,36 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const router = express.Router();
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const router = express.Router()
 
 // CORS 허용 설정
-app.use(cors());
+app.use(cors())
 
 // 라우터 정의
 router.get('/', function(req, res, next) {
   res.send('Hello from Express'); // 템플릿 엔진 대신 send 사용
-});
+})
 
 router.get('/api/get/nodejs-api', function(req, res) {
   res.status(200).json({
     "message": "hello get api nodejs-api"
-  });
-});
+  })
+})
 
-router.post('/api/post/nodejs-api', function(req, res) {
-  res.status(200).json({
-    "message": "hello post api nodejs-api"
-  });
-});
+router.post('/api/post/login', function(req, res) {
+  const { accessId, secretPw } = req.body
+  if (accessId == "admin" && secretPw == "1234") {
+    res.status(200).json({
+      "message": "login success."
+    })
+  } else {
+    res.status(200).json({
+      "message": "login fail."
+    })
+  }
+})
 
 // 라우터 등록
-app.use('/', router);
+app.use('/', router)
 
 module.exports = app
