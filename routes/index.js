18c98fcf0870a945694ec8eb94631d14195b4260
suinbo <suin9610@gmail.com>
2023-07-16 00:42:30 +0900

    [Feat] body-parser 미들웨어 연동 및 router 적용

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+
+// body-parser 미들웨어 사용: PUT 요청 본문을 파싱 (req.body)
+const bodyParser = require('body-parser');
+//app.use(express.json());
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 const router = express.Router();
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
 
 // CORS 허용 설정
 app.use(cors());
 
-// mysql 연동
-const usersRouter = require('./adminRouter');
-app.use(`/admins`, usersRouter);
+// '/admins' 경로로 들어오는 요청에 대해서만 router 동작
+const adminRouter = require('./adminRouter');
+app.use(`/admins`, adminRouter);
 
 // 라우터 정의
 router.get('/', function (req, res, next) {
