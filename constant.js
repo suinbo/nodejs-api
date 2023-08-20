f69583accce8215376ffc30d97f10328a0ebf0d9
suinbo <suin9610@gmail.com>
2023-08-20 21:47:38 +0900

    [Chore] 코드 상수명 변경

--- constant.js
+++ constant.js
@@ -1,6 +1,6 @@
 const SUCCESS_CODE = '0000',
     SESSION_EXPIRED_CODE = '1000',
-    NOT_EXISTED = '1100',
+    NOT_EXISTED_CODE = '1100',
     ERROR_CODE = '1111';
 
 const RESPONSE = {
@@ -17,11 +17,11 @@ const RESPONSE = {
         detailMessage: '세션이 만료되었습니다.',
     },
     UNAUTHENTICATED: {
-        code: NOT_EXISTED,
+        code: NOT_EXISTED_CODE,
         detailMessage: '존재하지 않는 계정입니다.',
     },
     UNCORRECT: {
-        code: NOT_EXISTED,
+        code: NOT_EXISTED_CODE,
         detailMessage: '비밀번호가 일치하지 않습니다.',
     },
     LOGIN_SUCCESS: {
